26c9ff0d16e5a91b7ec0fca4a73213ff59f8bb3a
飞翔 <70909689+hengdactn@users.noreply.github.com>
2025-11-22 18:45:01 +0800

    Update login.js

--- login.js
+++ login.js
@@ -13,7 +13,7 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
 
     try {
-      await page.goto('https://www.mediafire.com/login/');
+      await page.goto('https://www.cloudns.net/');
 
       // 等待页面加载完成
       await page.waitForTimeout(10000); // 增加等待时间，等待页面加载完全
